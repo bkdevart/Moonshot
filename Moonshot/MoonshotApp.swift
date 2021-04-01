ca797ecfc8103094a3050f1c0c9549f92dfca87d
Brandon Knox <bknox.dev@gmail.com>
2021-03-31 18:02:16 -0700

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Brandon Knox on 3/31/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
