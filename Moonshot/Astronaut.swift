3cf829ac0d94d57ca3969e3b8c6cc083907538a5
Brandon Knox <bknox.dev@gmail.com>
2021-04-01 05:28:36 -0700

    Added asset files  (json, images).  Created files to decode json and made basic view showing count of items in file.

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Brandon Knox on 4/1/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
