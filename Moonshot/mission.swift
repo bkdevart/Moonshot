821188559e35c1e198f4960604fe456582f89b98
Brandon Knox <bknox.dev@gmail.com>
2021-04-02 21:18:54 -0700

    Created MissionView

---
+++ Moonshot/mission.swift
@@ -0,0 +1,38 @@
+//
+//  mission.swift
+//  Moonshot
+//
+//  Created by Brandon Knox on 4/1/21.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        if let launchDate = launchDate {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .long
+            return formatter.string(from: launchDate)
+        } else {
+            return "N/A"
+        }
+    }
+}
